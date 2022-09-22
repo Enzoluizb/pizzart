ecad25678a67e846552d5548b7672fea51a19d5a
Enzo <enzoluiz.brt@gmail.com>
2022-09-22 15:32:29 -0300

    adicionando itens na tela de finalizar pedido

--- navegacao/pedido.php
+++ navegacao/pedido.php
@@ -2,34 +2,36 @@
 $title = "Pizzart | Finalizar pedido";
 include "header.php";
 
-$pedido=true;
+$pedido = true;
 $valor = 0;
 
 if (!isset($_POST['tipo_massa']))
-   $pedido = false;
+    $pedido = false;
 else if (!isset($_POST['tipo_tamanho']))
-   $pedido = false;
+    $pedido = false;
 else if (!isset($_POST['tipo_molho']))
-   $pedido = false;
+    $pedido = false;
 else if (!isset($_POST['tipo_queijo']))
-   $pedido = false;
+    $pedido = false;
 else if (!isset($_POST['tipo_carne']))
-   $pedido = false;
+    $pedido = false;
 else if (!isset($_POST['tipo_complemento']))
-   $pedido = false;
+    $pedido = false;
+else if (!isset($_POST['tipo_bebida']))
+    $pedido = false;
 
 
-if ($pedido){
-    if(empty($_SESSION['nome'])){
+if ($pedido) {
+    if (empty($_SESSION['nome'])) {
         header('location:login.php');
     } else {
-         
+
         if ($_POST['tipo_massa'] == 'Tradicional') {
-        $valor = $valor + 10;
-        } 
+            $valor = $valor + 10;
+        }
         if ($_POST['tipo_massa'] == 'Integral') {
-        $valor = $valor + 12;
-        } 
+            $valor = $valor + 12;
+        }
 
         if ($_POST['tipo_tamanho'] == 'Pequena') {
             $valor = $valor + 20;
@@ -40,7 +42,7 @@ if ($pedido){
         if ($_POST['tipo_tamanho'] == 'Grande') {
             $valor = $valor + 40;
         }
-        
+
         if ($_POST['tipo_molho'] == 'Tomate') {
             $valor = $valor + 10;
         }
@@ -50,7 +52,7 @@ if ($pedido){
         if ($_POST['tipo_molho'] == 'Pesto') {
             $valor = $valor + 15;
         }
-        
+
         if ($_POST['tipo_queijo'] == 'Gorgonzola') {
             $valor = $valor + 15;
         }
@@ -60,7 +62,7 @@ if ($pedido){
         if ($_POST['tipo_queijo'] == 'Parmesão') {
             $valor = $valor + 10;
         }
-        
+
         if ($_POST['tipo_carne'] == 'Bacon') {
             $valor = $valor + 15;
         }
@@ -70,7 +72,7 @@ if ($pedido){
         if ($_POST['tipo_carne'] == 'Frango') {
             $valor = $valor + 10;
         }
-        
+
         if ($_POST['tipo_complemento'] == 'Cebola') {
             $valor = $valor + 10;
         }
@@ -80,7 +82,7 @@ if ($pedido){
         if ($_POST['tipo_complemento'] == 'Tomate Cereja') {
             $valor = $valor + 10;
         }
-        
+
         if ($_POST['tipo_bebida'] == 'COCA-COLA 2L') {
             $valor = $valor + 12;
         }
@@ -94,6 +96,8 @@ if ($pedido){
 } else {
     //echo "falta complementos";
 }
+
+
 
 // echo '<pre>';
 // print_r($_POST);
@@ -113,28 +117,45 @@ if ($pedido){
 
 <table>
     <tr>
-        <th>Você escolheu o massa: <?php echo isset($_POST['tipo_massa'])?$_POST['tipo_massa']:"Não informado"?></th>
+        <th>Você escolheu o massa: <?php echo isset($_POST['tipo_massa']) ? $_POST['tipo_massa'] : "Não informado" ?></th>
     </tr>
     <tr>
-        <th>Você escolheu o tamanho: <?php echo isset($_POST['tipo_tamanho'])?$_POST['tipo_tamanho']:"Não informado"?></th>
+        <th>Você escolheu o tamanho: <?php echo isset($_POST['tipo_tamanho']) ? $_POST['tipo_tamanho'] : "Não informado" ?></th>
     </tr>
     <tr>
-        <th>Você escolheu o molho: <?php echo isset($_POST['tipo_molho'])?$_POST['tipo_molho']:"Não informado"?></th>
+        <th>Você escolheu o molho: <?php echo isset($_POST['tipo_molho']) ? $_POST['tipo_molho'] : "Não informado" ?></th>
     </tr>
     <tr>
-        <th>Você escolheu o queijo: <?php echo isset($_POST['tipo_queijo'])?$_POST['tipo_queijo']:"Não informado"?></th>
+        <th>Você escolheu o queijo: <?php echo isset($_POST['tipo_queijo']) ? $_POST['tipo_queijo'] : "Não informado" ?></th>
     </tr>
     <tr>
-        <th>Você escolheu a carne: <?php echo isset($_POST['tipo_carne'])?$_POST['tipo_carne']:"Não informado"?></th>
+        <th>Você escolheu a carne: <?php echo isset($_POST['tipo_carne']) ? $_POST['tipo_carne'] : "Não informado" ?></th>
     </tr>
     <tr>
-        <th>Você escolheu o complemento: <?php echo isset($_POST['tipo_complemento'])?$_POST['tipo_complemento']:"Não informado"?></th>
+        <th>Você escolheu o complemento: <?php echo isset($_POST['tipo_complemento']) ? $_POST['tipo_complemento'] : "Não informado" ?></th>
     </tr>
     <tr>
-        <th>Você escolheu a bebida: <?php echo isset($_POST['tipo_bebida'])?$_POST['tipo_bebida']:"Não informado"?></th>
+        <th>Você escolheu a bebida: <?php echo isset($_POST['tipo_bebida']) ? $_POST['tipo_bebida'] : "Não informado" ?></th>
     </tr>
     <tr>
         <th>Preço total: <?php echo $valor; ?></th>
+    </tr>
+    <tr>
+        <th>
+            <select name="select">
+                <option value="valor1" selected>Pix</option>
+                <option value="valor2">Cartão</option>
+                <option value="valor3">Dinheiro</option>
+            </select>
+        </th>
+    </tr>
+    <tr>
+        <th>
+            Endereço: <input type="text">
+        </th>
+    </tr>
+    <tr>
+        <th><button style="padding: 5px; border: none; background: #bdbdbd; color: black; border-radius: 5px;">Finalizar pedido</button></th>
     </tr>
 </table>
 
