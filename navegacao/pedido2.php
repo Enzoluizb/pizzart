27d6c9623bdbe82e4edb8f8e5220bcb9dac1ee7f
Enzo <enzoluiz.brt@gmail.com>
2022-09-23 16:11:54 -0300

    adicionando margin-bottom na pg pedido2

--- navegacao/pedido2.php
+++ navegacao/pedido2.php
@@ -104,7 +104,7 @@ if ($pedido) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head> -->
 
-<table>
+<table style="margin-bottom: 310px;">
     <tr>
         <th>
             Sua arte está pronta!
